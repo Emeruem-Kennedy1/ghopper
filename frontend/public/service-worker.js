a8997d03e2bf5b6f95edcb58a28ea6e8fb578235
Emeruem-Kennedy1 <kennedyemeruem123@gmail.com>
2025-04-05 00:00:08 -0700

    service worker fix

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -7,6 +7,10 @@ const urlsToCache = [
   // Add other static assets here
 ];
 
+// This line is required for vite-plugin-pwa
+// eslint-disable-next-line no-unused-vars
+const manifestFiles = self.__WB_MANIFEST;
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
