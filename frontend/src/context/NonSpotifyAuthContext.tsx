a86727e9a60a9c98316ac0e88e08a9aea5a1a9fe
Emeruem Kennedy <69385717+Emeruem-Kennedy1@users.noreply.github.com>
2025-04-04 23:54:10 -0700

    Revert "fix: cleanup and PWA (#35)" (#36)
    This reverts commit c29df36f180e29e29979a675ddbbe87bbf5c8b72.

--- frontend/src/context/NonSpotifyAuthContext.tsx
+++ frontend/src/context/NonSpotifyAuthContext.tsx
@@ -1,3 +1,4 @@
+// frontend/src/context/NonSpotifyAuthContext.tsx
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -16,18 +15,7 @@ export default defineConfig(({ mode }) => {
   console.log(`Mode: ${mode}, Backend URL: ${backendUrl}`); // For debugging
 
   return {
-    plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      strategies: 'injectManifest',
-      srcDir: 'public',
-      filename: 'service-worker.js',
-      includeAssets: ['favicon.ico', 'app-icon.svg', 'robots.txt'],
-      manifest: false // We're using our own manifest.json
-    })
-    ],
+    plugins: [react()],
     server: {
       host: "0.0.0.0",
       port: 3000,
